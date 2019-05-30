517e8acf43ee459eba588867a15be721d06f5a5d
tjohnny93 <tjohnny93@berkeley.edu>
2019-05-30 15:40:27 -0700

    Complete seeds info

--- polls_app/db/seeds.rb
+++ polls_app/db/seeds.rb
@@ -9,4 +9,18 @@
 user1 = User.create([{ username: 'Joe' }])
 user2 = User.create([{ username: 'Johnny' }])
 
-# poll1 = Poll.createcreate([{ title:  }])
+poll1 = Poll.create([{ title:  "PIZZA", user_id: 1}])
+poll2 = Poll.create([{ title:  "Commute", user_id: 2}])
+
+question1 = Question.create([{ text: "Who wants Pizza?", poll_id: 1}])
+question2 = Question.create([{ text: "Who takes the bus?", poll_id: 2}])
+
+answer1 = AnswerChoices.create([{ answer_text: "Yes!!!", question_id: 1 }])
+answer2 = AnswerChoices.create([{ answer_text: "YUCKY!", question_id: 1 }])
+answer3 = AnswerChoices.create([{ answer_text: "I do", question_id: 2 }])
+answer4 = AnswerChoices.create([{ answer_text: "Not me", question_id: 2 }])
+
+response1 = Response.create([{ answerchoice_id: 1, user_id: 1 }])
+response2 = Response.create([{ answerchoice_id: 2, user_id: 2 }])
+response3 = Response.create([{ answerchoice_id: 3, user_id: 1 }])
+response4 = Response.create([{ answerchoice_id: 4, user_id: 2 }])
