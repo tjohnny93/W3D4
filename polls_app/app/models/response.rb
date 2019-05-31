2b7a3e6fb6538125d27bd895236fe23be0c9d5fa
tjohnny93 <tjohnny93@berkeley.edu>
2019-05-30 17:30:40 -0700

    Add validate method in response model

--- polls_app/app/models/response.rb
+++ polls_app/app/models/response.rb
@@ -10,13 +10,38 @@
 #
 
 class Response < ApplicationRecord
+  validate :respondent_already_answered?
+  
   belongs_to :answer_choice,
-    primary_key: :id,
-    foreign_key: :answerchoice_id,
-    class_name: :AnswerChoice
-
+  primary_key: :id,
+  foreign_key: :answerchoice_id,
+  class_name: :AnswerChoice
+  
   belongs_to  :respondent,
-    primary_key: :id,
-    foreign_key: :user_id,
-    class_name: :User
+  primary_key: :id,
+  foreign_key: :user_id,
+  class_name: :User
+  
+  has_one :question,
+  through: :answer_choice,
+  source: :question   
+  
+  
+  def sibling_responses
+      question = self.question
+      same_id = self.id
+      question.responses.where.not(responses: {id: same_id})
+  end
+
+  # private ## ask question
+
+  def respondent_already_answered?
+    respondent_id = self.sibling_responses.pluck(:user_id)
+    if respondent_id.include?(self.user_id)
+      errors[:response] << "already responded"
+    end
+  end
+
+  
+
 end
--- polls_app/app/models/user.rb
+++ polls_app/app/models/user.rb
@@ -9,6 +9,8 @@
 #
 
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+
   has_many :authored_polls,
     primary_key: :id,
     foreign_key: :user_id,
